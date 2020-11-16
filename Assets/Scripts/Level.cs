b3948519daeae5ddea4631e0a9c03be9807ce0b4
Richard Lawson <51132990+lawsonarichard@users.noreply.github.com>
2020-11-15 17:33:30 -0800

    added block count per level
    added block count per level

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int breakableBlocks; // Seralized for debugging purposes
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++; // add 1
+    }
+
+}
