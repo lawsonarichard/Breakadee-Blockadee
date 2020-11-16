bf18571f093c2200190f0a36dba92f2df4f5e66d
Richard Lawson <51132990+lawsonarichard@users.noreply.github.com>
2020-11-15 17:46:25 -0800

    allow level completion
    allow level completion

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
+    // params
     [SerializeField] int breakableBlocks; // Seralized for debugging purposes
+    
+    // cached reference
+    SceneLoader sceneloader;
+
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();
+    }
 
     public void CountBreakableBlocks()
     {
         breakableBlocks++; // add 1
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneloader.LoadNextScene();
+        }
     }
 
 }
