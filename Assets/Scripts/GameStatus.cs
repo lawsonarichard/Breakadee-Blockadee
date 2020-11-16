fc8a1467827e01beef6ebe81e6e18f63376c7f9e
Richard Lawson <51132990+lawsonarichard@users.noreply.github.com>
2020-11-15 18:41:50 -0800

    level 2
    level 2 and score creation

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class GameStatus : MonoBehaviour
 {
     // params
     [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // state vars
     [SerializeField] int currentScore = 0;
 
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -20,6 +25,7 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore = currentScore + pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 
 }
